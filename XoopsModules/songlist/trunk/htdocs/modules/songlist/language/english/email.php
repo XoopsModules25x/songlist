b97e626a3a36cee80776adb646a4c2313e0e4f4b
wishcraft <wishcraft@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-03-25 22:42:30 +0000

    Karioki Song List 1.07 - complete first release final

---
+++ XoopsModules/songlist/trunk/htdocs/modules/songlist/language/english/email.php
@@ -0,0 +1,8 @@
+<?php
+
+	// Requests
+	define('_MN_SONGLIST_SUBJECT_REQUESTMADE', '[#%s] - A Request for a song to be added to the song list has been recieved!');
+	define('_MN_SONGLIST_SUBJECT_REQUESTFOUND', '[#%s] - Your Request for a song to be added to the song list has been actioned!');
+	
+	
+?>
